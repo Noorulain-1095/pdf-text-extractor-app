e24d4e104018400def1be8f916b4349391959fd7
Sh-Noorulain <dev.noorulain@gmail.com>
2024-06-03 00:07:40 +0500

    Applied CORS to prevent CSRF attacks

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,14 @@ import pdfRoutes from './routes/pdfRoutes';
 
 const app = express();
 
-// Enables Cross-Origin Resource Sharing (CORS) for all routes
-app.use(cors());
+// Allow requests only from the target domain
+const corsOptions = {
+    origin: 'http://localhost:3001', // Replace with target domain
+    optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
+  };
+  
+// Note: In future we can implement custom middleware for additional Server-Side Checks
+app.use(cors(corsOptions));
 
 // Parses incoming JSON requests and puts the parsed data in req.body
 app.use(express.json());
